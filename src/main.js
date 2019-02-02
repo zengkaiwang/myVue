e98c951e2a6d5f41217a945fbc7e04a5a0a0a7df
wangzengkai <wangzengkai@zuoyebang.com>
2019-02-02 17:07:15 +0800

    在main.js中import引入的css样式编译后会放在html中的style标签下

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import ElementUI from 'zyb-pc-ui';
-import 'zyb-pc-ui/lib/theme-chalk/index.css';
+// import 'zyb-pc-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
 
