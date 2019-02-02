7a63a6ac3e84d52e73fcae92511e5e49f5a949b3
wangzengkai <wangzengkai@zuoyebang.com>
2019-02-02 17:03:12 +0800

    测试zyb-pc-ui的配置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'zyb-pc-ui';
+import 'zyb-pc-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
 new Vue({
