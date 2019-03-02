e3eec166db303d88985a56c4d70feda0c4628a0c
wangzengkai <wangzengkai@zuoyebang.com>
2019-03-02 19:25:31 +0800

    提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,8 @@
     <h1>{{ msg }}</h1>
     <div class="test">
       <el-button type="primary" round>主要按钮</el-button>
+      <button @click="test">加载第三方组件</button>
+      <div id="mycomponent"></div>
     </div>
   </div>
 </template>
@@ -12,6 +14,24 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  created() {
+
+  },
+  methods: {
+    test() {
+      const s1 = document.createElement('script');
+      s1.type = 'text/javascript';
+      s1.src = 'https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.2.0/webcomponents-loader.js';
+      document.body.appendChild(s1);
+        
+      var script = document.createElement('script');
+      script.src = './my-custom-element.min.js';
+      script.type = 'text/javascript';
+      document.body.appendChild(script);
+
+      document.getElementById('mycomponent').innerHTML= '<my-custom-element></my-custom-element>'
+    }
   }
 }
 </script>
